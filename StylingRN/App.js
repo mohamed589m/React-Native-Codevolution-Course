a64a30f05210c176e8ac1bd1206fb0eedd4f7820
mohamed589m <mohamed53634372@gmail.com>
2025-01-30 17:02:43 +0200

    update

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -1,21 +1,55 @@
 import { View, Text, StyleSheet } from "react-native";
 
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>StyleSheet API</Text>
+//     </View>
+//   );
+// }
+
+// export const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: "plum",
+//     flex: 1,
+//     padding: 60,
+//   },
+//   title: {
+//     textAlign: "center",
+//     fontFamily: "cursive",
+//   },
+// });
+
+// ******************************************************************************* //
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>StyleSheet API</Text>
+      <View style={[styles.box, styles.lightblueBg]}>
+        <Text>Light blue box</Text>
+      </View>
+      <View style={[styles.box, styles.lightgreenBg]}>
+        <Text>Light green box</Text>
+      </View>
     </View>
   );
 }
 
-export const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     backgroundColor: "plum",
     flex: 1,
     padding: 60,
   },
-  title: {
-    textAlign: "center",
-    fontFamily: "cursive",
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
   },
 });
