9fb5998bfb9cb6e24b757d519a24f0bb3b5e61f2
mohamed589m <mohamed53634372@gmail.com>
2025-01-30 21:56:29 +0200

    update

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -22,10 +22,44 @@ import { View, Text, StyleSheet } from "react-native";
 
 // ******************************************************************************* //
 
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={[styles.box, styles.lightblueBg]}>
+//         <Text>Light blue box</Text>
+//       </View>
+//       <View style={[styles.box, styles.lightgreenBg]}>
+//         <Text>Light green box</Text>
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: "plum",
+//     flex: 1,
+//     padding: 60,
+//   },
+//   box: {
+//     width: 100,
+//     height: 100,
+//     padding: 10,
+//   },
+//   lightblueBg: {
+//     backgroundColor: "lightblue",
+//   },
+//   lightgreenBg: {
+//     backgroundColor: "lightgreen",
+//   },
+// });
+
+// ******************************************************************************* //
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={[styles.box, styles.lightblueBg]}>
+      <View style={[styles.box, styles.lightblueBg, styles.androidShadow]}>
         <Text>Light blue box</Text>
       </View>
       <View style={[styles.box, styles.lightgreenBg]}>
@@ -42,14 +76,40 @@ const styles = StyleSheet.create({
     padding: 60,
   },
   box: {
-    width: 100,
-    height: 100,
-    padding: 10,
+    width: "100%",
+    height: "25%",
+    // padding: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    // margin: 10,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: "purple",
+
+    // * Border radius is applicable to view component across both platforms (android and ios) but for text component it only applies to android
+
+    borderRadius: 5,
   },
   lightblueBg: {
     backgroundColor: "lightblue",
   },
   lightgreenBg: {
     backgroundColor: "lightgreen",
+  },
+  //* Apply only on ios
+  boxShadow: {
+    shadowColor: "black",
+    shadowOffset: {
+      width: 6,
+      height: 6,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 4,
+  },
+  //****************************** */
+  //* To make shadow apply on android
+  androidShadow: {
+    elevation: 10,
+    shadowColor:"gray" // The only shadow property that work on android
   },
 });
