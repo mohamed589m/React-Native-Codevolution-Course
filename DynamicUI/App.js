2694ff75307a9c8a5332e6ebde0cd65c8798590f
mohamed589m <mohamed53634372@gmail.com>
2025-02-04 16:53:12 +0200

    update

--- DynamicUI/App.js
+++ DynamicUI/App.js
@@ -1,59 +1,113 @@
-import { useEffect, useState } from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+// import { useEffect, useState } from "react";
+// import { Dimensions, StyleSheet, Text, View } from "react-native";
+
+// export default function App() {
+//   const [dimensions, setDimensions] = useState({
+//     window: Dimensions.get("window"),
+//   });
+
+//   useEffect(() => {
+//     const subscription = Dimensions.addEventListener("change", ({ window }) =>
+//       setDimensions({ window })
+//     );
+//     return () => subscription?.remove();
+//   });
+
+//   const { window } = dimensions;
+//   const windowWidth = window.width;
+//   const windowHeight = window.height;
+
+//   return (
+//     <View style={styles.container}>
+//       <View
+//         style={[
+//           styles.box,
+//           {
+//             width: windowWidth > 500 ? "70%" : "90%",
+//             height: windowHeight > 600 ? "60%" : "90%",
+//           },
+//         ]}
+//       >
+//         <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome!</Text>
+//       </View>
+//     </View>
+//   );
+// }
+
+// *****************************************************************************************************
+
+// import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
+
+// export default function App() {
+//   const windowWidth = useWindowDimensions().width;
+//   const windowHeight = useWindowDimensions().height;
+
+//   return (
+//     <View style={styles.container}>
+//       <View
+//         style={[
+//           styles.box,
+//           {
+//             width: windowWidth > 500 ? "70%" : "90%",
+//             height: windowHeight > 600 ? "60%" : "90%",
+//           },
+//         ]}
+//       >
+//         <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome!</Text>
+//       </View>
+//     </View>
+//   );
+// }
+
+// // const windowWidth = Dimensions.get("window").width;
+// // const windowHeight = Dimensions.get("window").height;
+
+// // console.log({ windowWidth, windowHeight });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "plum",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   box: {
+//     // width: windowWidth > 500 ? "70%" : "90%",
+//     // height: windowHeight > 600 ? "60%" : "90%",
+//     backgroundColor: "lightblue",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   // text: {
+//   //   fontSize: windowWidth > 500 ? 50 : 24,
+//   // },
+// });
+
+// *****************************************************************************************************
+
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
 
 export default function App() {
-  const [dimensions, setDimensions] = useState({
-    window: Dimensions.get("window"),
-  });
-
-  useEffect(() => {
-    const subscription = Dimensions.addEventListener("change", ({ window }) =>
-      setDimensions({ window })
-    );
-    return () => subscription?.remove();
-  });
-
-  const { window } = dimensions;
-  const windowWidth = window.width;
-  const windowHeight = window.height;
-
   return (
     <View style={styles.container}>
-      <View
-        style={[
-          styles.box,
-          {
-            width: windowWidth > 500 ? "70%" : "90%",
-            height: windowHeight > 600 ? "60%" : "90%",
-          },
-        ]}
-      >
-        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome!</Text>
+      <View style={styles.box}>
+        <Text style={styles.text}>Welcome</Text>
       </View>
     </View>
   );
 }
 
-// const windowWidth = Dimensions.get("window").width;
-// const windowHeight = Dimensions.get("window").height;
-
-// console.log({ windowWidth, windowHeight });
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "plum",
-    alignItems: "center",
-    justifyContent: "center",
   },
   box: {
-    // width: windowWidth > 500 ? "70%" : "90%",
-    // height: windowHeight > 600 ? "60%" : "90%",
-    backgroundColor: "lightblue",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
   },
-  // text: {
-  //   fontSize: windowWidth > 500 ? 50 : 24,
-  // },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 });
