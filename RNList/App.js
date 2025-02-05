bb88f82fffc8dac9b3633dc978b84014078119ee
mohamed589m <mohamed53634372@gmail.com>
2025-02-05 16:58:05 +0200

    update

--- RNList/App.js
+++ RNList/App.js
@@ -90,67 +90,67 @@ which renders only the items currently in view making it highly performant for l
 
 // ************************************************************************************************
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.scrollView}>
-        <FlatList
-          data={PokemonList}
-          // contentContainerStyle={styles.flatListStyle}
-          renderItem={({ item }) => {
-            // the parameter must called item
-            // console.log(item.id);
-            return (
-              <View key={item.id} style={styles.card}>
-                <Text style={styles.cardText}>{item.type}</Text>
-                <Text style={styles.cardText}>{item.name}</Text>
-              </View>
-            );
-          }}
-          // horizontal // If you want to render the list horizontally
-          // horizontal={false}
+// export default function App() {
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <View style={styles.scrollView}>
+//         <FlatList
+//           data={PokemonList}
+//           //  {...data.length==0?contentContainerStyle={styles.flatListStyle}:null}
+//           renderItem={({ item }) => {
+//             // the parameter must called item
+//             // console.log(item.id);
+//             return (
+//               <View key={item.id} style={styles.card}>
+//                 <Text style={styles.cardText}>{item.type}</Text>
+//                 <Text style={styles.cardText}>{item.name}</Text>
+//               </View>
+//             );
+//           }}
+//           // horizontal // If you want to render the list horizontally
+//           // horizontal={false}
 
-          keyExtractor={(item, index) => item.id.toString()}
-          ItemSeparatorComponent={<View style={{ height: 16 }} />} //It is the component between each item in the list and we use it here instead of marginBottom that makes a marginBottom for the last element too
-          ListEmptyComponent={
-            <Text style={styles.emptyText}>No items found</Text>
-          } //this prop accepts a react component and renders it when the list is empty
-        />
-      </View>
-    </SafeAreaView>
-  );
-}
+//           keyExtractor={(item, index) => item.id.toString()}
+//           ItemSeparatorComponent={<View style={{ height: 16 }} />} //It is the component between each item in the list and we use it here instead of marginBottom that makes a marginBottom for the last element too
+//           ListEmptyComponent={
+//             <Text style={styles.emptyText}>No items found</Text>
+//           } //this prop accepts a react component and renders it when the list is empty
+//         />
+//       </View>
+//     </SafeAreaView>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f5f5f5",
-    paddingTop: StatusBar.currentHeight, // SafeAreaView on ios will not affected by this
-  },
-  scrollView: {
-    paddingHorizontal: 16,
-    flex: 1,
-  },
-  card: {
-    backgroundColor: "white",
-    padding: 16,
-    borderRadius: 8,
-    borderWidth: 1,
-    // marginBottom: 16,
-  },
-  cardText: {
-    fontSize: 30,
-  },
-  flatListStyle: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  emptyText: {
-    fontSize: 20,
-    color: "gray",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#f5f5f5",
+//     paddingTop: StatusBar.currentHeight, // SafeAreaView on ios will not affected by this
+//   },
+//   scrollView: {
+//     paddingHorizontal: 16,
+//     flex: 1,
+//   },
+//   card: {
+//     backgroundColor: "white",
+//     padding: 16,
+//     borderRadius: 8,
+//     borderWidth: 1,
+//     // marginBottom: 16,
+//   },
+//   cardText: {
+//     fontSize: 30,
+//   },
+//   flatListStyle: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   emptyText: {
+//     fontSize: 20,
+//     color: "gray",
+//   },
+// });
 
 // ************************************************************************************************
 // *When list is empty
@@ -209,3 +209,79 @@ const styles = StyleSheet.create({
 //     color: "gray",
 //   },
 // });
+
+// ************************************************************************************************
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.scrollView}>
+        <FlatList
+          data={PokemonList}
+          renderItem={({ item }) => {
+            // the parameter must called item
+            // console.log(item.id);
+            return (
+              <View key={item.id} style={styles.card}>
+                <Text style={styles.cardText}>{item.type}</Text>
+                <Text style={styles.cardText}>{item.name}</Text>
+              </View>
+            );
+          }}
+          // horizontal // If you want to render the list horizontally
+          // horizontal={false}
+
+          keyExtractor={(item, index) => item.id.toString()}
+          ItemSeparatorComponent={<View style={{ height: 16 }} />} //It is the component between each item in the list and we use it here instead of marginBottom that makes a marginBottom for the last element too
+          ListEmptyComponent={
+            <Text style={styles.emptyText}>No items found</Text>
+          } //this prop accepts a react component and renders it when the list is empty
+
+          ListHeaderComponent={
+            <Text style={styles.headerText}>Pokemon List</Text>
+          }
+
+          ListFooterComponent={
+            <Text style={styles.footerText}>End of list</Text>
+          }
+
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: StatusBar.currentHeight, // SafeAreaView on ios will not affected by this
+  },
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    // marginBottom: 16,
+  },
+  cardText: {
+    fontSize: 30,
+  },
+  emptyText: {
+    fontSize: 20,
+    color: "gray",
+  },
+  headerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  footerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginTop: 12,
+  },
+});
