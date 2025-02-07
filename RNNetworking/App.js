baeac3cde638e8089d4c5da4496f1a429410ee8b
mohamed589m <mohamed53634372@gmail.com>
2025-02-07 20:06:49 +0200

    update

--- RNNetworking/App.js
+++ RNNetworking/App.js
@@ -105,14 +105,132 @@
 // *******************************************************************************
 // *******************************Pull down to refresh***************************
 
+// import { useEffect, useState } from "react";
+// import {
+//   ActivityIndicator,
+//   FlatList,
+//   SafeAreaView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   View,
+// } from "react-native";
+
+// export default function App() {
+//   const [postList, setPostList] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [refreshing, setRefreshing] = useState(false);
+
+//   const fetchData = async (limit = 10) => {
+//     const response = await fetch(
+//       `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+//     );
+//     const data = await response.json();
+//     setPostList(data);
+//     setIsLoading(false);
+//   };
+
+//   const handleRefresh = () => {
+//     setRefreshing(true);
+//     fetchData(20);
+//     setRefreshing(false);
+//   };
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   if (isLoading) {
+//     return (
+//       <SafeAreaView style={styles.loadingContainer}>
+//         <ActivityIndicator size="large" color="#0000ff" />
+//         <Text>Loading...</Text>
+//       </SafeAreaView>
+//     );
+//   }
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <View style={styles.listContainer}>
+//         <FlatList
+//           data={postList}
+//           renderItem={({ item }) => {
+//             return (
+//               <View style={styles.card}>
+//                 <Text style={styles.titleText}>{item.title}</Text>
+//                 <Text style={styles.bodyText}>{item.body}</Text>
+//               </View>
+//             );
+//           }}
+//           ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+//           ListEmptyComponent={<Text>No posts fount</Text>}
+//           ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
+//           ListFooterComponent={
+//             <Text style={styles.footerText}>End of list</Text>
+//           }
+//           refreshing={refreshing}
+//           onRefresh={handleRefresh}
+//         />
+//       </View>
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#f5f5f5",
+//     paddingTop: StatusBar.currentHeight,
+//   },
+//   listContainer: {
+//     flex: 1,
+//     paddingHorizontal: 16,
+//   },
+//   card: {
+//     backgroundColor: "white",
+//     padding: 16,
+//     borderRadius: 8,
+//     borderWidth: 1,
+//   },
+//   titleText: {
+//     fontSize: 30,
+//   },
+//   bodyText: {
+//     fontSize: 24,
+//     color: "#666666",
+//   },
+//   headerText: {
+//     fontSize: 24,
+//     textAlign: "center",
+//     marginBottom: 12,
+//   },
+//   footerText: {
+//     fontSize: 24,
+//     textAlign: "center",
+//     marginTop: 12,
+//   },
+//   loadingContainer: {
+//     flex: 1,
+//     backgroundColor: "#F5F5F5",
+//     paddingTop: StatusBar.currentHeight,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+// });
+
+// *******************************************************************************
+// *******************************Post Request***************************
+
 import { useEffect, useState } from "react";
 import {
   ActivityIndicator,
+  Button,
   FlatList,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from "react-native";
 
@@ -120,6 +238,9 @@ export default function App() {
   const [postList, setPostList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [isPosting, setIsPosting] = useState(false);
 
   const fetchData = async (limit = 10) => {
     const response = await fetch(
@@ -134,6 +255,26 @@ export default function App() {
     setRefreshing(true);
     fetchData(20);
     setRefreshing(false);
+  };
+
+  const addPost = async () => {
+    setIsPosting(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody,
+      }),
+    });
+
+    const newPost = await response.json();
+    setPostList([newPost, ...postList]);
+    setPostTitle("");
+    setPostBody("");
+    setIsPosting(false);
   };
 
   useEffect(() => {
@@ -151,27 +292,50 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.listContainer}>
-        <FlatList
-          data={postList}
-          renderItem={({ item }) => {
-            return (
-              <View style={styles.card}>
-                <Text style={styles.titleText}>{item.title}</Text>
-                <Text style={styles.bodyText}>{item.body}</Text>
-              </View>
-            );
-          }}
-          ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
-          ListEmptyComponent={<Text>No posts fount</Text>}
-          ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
-          ListFooterComponent={
-            <Text style={styles.footerText}>End of list</Text>
-          }
-          refreshing={refreshing}
-          onRefresh={handleRefresh}
-        />
-      </View>
+      <>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Post title"
+            value={postTitle}
+            onChangeText={setPostTitle}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Post body"
+            value={postBody}
+            onChangeText={setPostBody}
+          />
+          <Button
+            title={isPosting ? "Adding..." : "Add Post"}
+            onPress={addPost}
+            disabled={isPosting}
+          />
+        </View>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={postList}
+            renderItem={({ item }) => {
+              return (
+                <View style={styles.card}>
+                  <Text style={styles.titleText}>{item.title}</Text>
+                  <Text style={styles.bodyText}>{item.body}</Text>
+                </View>
+              );
+            }}
+            ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+            ListEmptyComponent={<Text>No posts fount</Text>}
+            ListHeaderComponent={
+              <Text style={styles.headerText}>Post List</Text>
+            }
+            ListFooterComponent={
+              <Text style={styles.footerText}>End of list</Text>
+            }
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+          />
+        </View>
+      </>
     </SafeAreaView>
   );
 }
@@ -215,5 +379,20 @@ const styles = StyleSheet.create({
     paddingTop: StatusBar.currentHeight,
     justifyContent: "center",
     alignItems: "center",
+  },
+  inputContainer: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 8,
   },
 });
