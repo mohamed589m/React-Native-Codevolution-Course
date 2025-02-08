f3a34b3e94fca60346e59a2e53c5888e0c6ce37e
mohamed589m <mohamed53634372@gmail.com>
2025-02-08 17:24:47 +0200

    update

--- RNNavigation/App.js
+++ RNNavigation/App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
+import { Alert, Pressable, Text } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
@@ -22,9 +23,34 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#6a51ae",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerRight: () => (
+            <Pressable onPress={() => alert("Menu button pressed!")}>
+              <Text style={{ color: "#fff", fontSize: 16 }}>Menu</Text>
+            </Pressable>
+          ),
+          contentStyle: {
+            backgroundColor: "#e8e4f3",
+          },
+        }}
+      >
         {/* // we uses initialRouteName to make the About screen displayed as the initial screen */}
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Welcome Home",
+          }}
+        />
         <Stack.Screen
           name="About"
           component={AboutScreen}
