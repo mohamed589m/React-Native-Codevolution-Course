33015b446f4cd465f9a3f9e31f1df840e5acdb3b
mohamed589m <mohamed53634372@gmail.com>
2025-02-08 23:13:32 +0200

    update

--- RNNavigation/App.js
+++ RNNavigation/App.js
@@ -2,18 +2,38 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import CourseListScreen from "./screens/CourseListScreen";
 import ProfileScreen from "./screens/ProfileScreen";
-import SettingsScreen from "./screens/SettingsScreen"
+import SettingsScreen from "./screens/SettingsScreen";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
-export default function App(){
-    return(
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Course List" component={CourseListScreen}/>
-                <Tab.Screen name="Profile" component={ProfileScreen}/>
-                <Tab.Screen name="Settings" component={SettingsScreen}/>
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarLabelPosition: "below-icon", //for mobile
+          // tabBarLabelPosition: "beside-icon", //for for tablet or ipad
+          // tabBarShowLabel:false  // to hide the tab label and display icons only
+          tabBarShowLabel: true, // to hide the tab label and display icons only
+          tabBarActiveTintColor: "purple",
+          tabBarInactiveTintColor: "green",
+        }}
+      >
+        <Tab.Screen name="Course List" component={CourseListScreen} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "My Profile",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="person" size={20} color={color} />
+            ),
+            tabBarBadge: 3,
+          }}
+        />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
